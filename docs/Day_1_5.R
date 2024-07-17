f5ecc3a99a3e8f3c81f2ba06f45f4fe17bd4df2d
Mader <sebastianmader@gmx.net>
2024-07-17 16:01:52 +0200

    worked through tutorial, added and adjusted

--- docs/Day_1_5.R
+++ docs/Day_1_5.R
@@ -1,12 +1,14 @@
 rm(list = ls())
 
-set.seed(1543)
-
 library(rstan)
 library(coda)
 
+set.seed(1543)
+
+# set settings for core usage by Stan
 rstan_options(auto_write = TRUE)
-options(mc.cores = parallel::detectCores())
+parallel::detectCores(all.test = FALSE, logical = TRUE)
+options(mc.cores = 3)
 
 n = 145
 
@@ -25,11 +27,13 @@ data = list(n = n, x = df$x, y = df$y)
 
 stan_code = '
 data {
+  // define structure of input data
   int n;
   vector[n] x;
   vector[n] y;
 }
 parameters{
+  // define structure of output data & set their boundaries
   real a; // intercept
   real b; // slope
   real<lower=0> sigma; // residual sdev
@@ -61,10 +65,14 @@ fit = sampling(stan_model, #
                chains=3)
 
 print(fit)
+# print(fit,
+#       digits=3,
+#       probs=c(0.05, 0.95),
+#       pars=c("a","b","sigma")
+# )
 
 plot(fit, pars=c("a", "b"))
 
-# stan_trace(fit)
 
 posterior = As.mcmc.list(fit)
 
@@ -74,3 +82,15 @@ plot(posterior[ , 1:3])
 
 library(BayesianTools)
 correlationPlot( as.matrix(fit)[, 1:3]   )
+
+# # Other plotting options
+# 
+# stan_plot(fit)
+# 
+# stan_trace(fit)
+# 
+# stan_hist(fit)
+# 
+# stan_dens(fit)
+# 
+# stan_scat(fit, pars = c("a", "b"))
